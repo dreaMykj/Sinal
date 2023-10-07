fc551bde0ea91f887e7cdf90767ad766d96f77a7
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-07 11:51:49 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numero;
+
+Console.Write("Digite um numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0) 
+{
+    Console.WriteLine("Positivo");
+}
+else if (numero < 0 )
+{
+    Console.WriteLine("Negativo");
+}
+else
+{
+    Console.WriteLine("Zero");
+}
